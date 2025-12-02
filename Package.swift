9cd7a4fe1a8c689f98ac26ac9dbc3bac2e204801
Samir Meslem <hello@samirmeslem.de>
2025-12-02 13:33:10 +0100

    Update llama.cpp to latest master for Phi-4 Mini support
    - Update llama.cpp from b6d6c5289 to 7f3a72a8e (latest master)
    - Adds support for 'gpt-4o' pre-tokenizer type (required for Phi-4 Mini)
    - Includes commit c43a3e799: "llama : add Phi-4-mini support"
    
    Fixes error: "unknown pre-tokenizer type: 'gpt-4o'"
    
    Changes since old version:
    - Phi-4 Mini model support added
    - Various CUDA, Metal, and Vulkan improvements
    - Bug fixes and performance optimizations
    
    Master commit (2024-12-02): 7f3a72a8ed796a135c81903546ef57c4aeda9f06

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .library(name: "SwiftLlama", targets: ["SwiftLlama"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ggerganov/llama.cpp.git", revision: "b6d6c5289f1c9c677657c380591201ddb210b649")
+        .package(url: "https://github.com/ggerganov/llama.cpp.git", revision: "7f3a72a8ed796a135c81903546ef57c4aeda9f06")
     ],
     targets: [
         .target(
